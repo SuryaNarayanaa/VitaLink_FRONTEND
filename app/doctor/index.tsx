ddd9b9feeee8734280a69164b1ea8039c7a7d688
keerti144 <keertiramkumar@gmail.com>
2025-03-29 12:01:00 +0530

    working

--- app/doctor/index.tsx
+++ app/doctor/index.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Routes, Route } from "react-router-dom";
+import PatientsPage from "./pages/PatientsPage";
+import PatientDetailPage from "./pages/PatientDetailPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
-const ViewPatients = () => {
-  return (
-    <View>
-      <Text>ViewPatients</Text>
-    </View>
-  )
-}
+const queryClient = new QueryClient();
 
-export default ViewPatients
+const ViewPatients = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <Routes>
+        <Route path="/" element={<PatientsPage />} />
+        <Route path="/patient/:id" element={<PatientDetailPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default ViewPatients;
