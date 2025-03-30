741c57965f5c45982f3e1aabd5273181d06bb132
Karthick1905 <jskarthick399@gmail.com>
2025-03-30 21:05:28 +0530

    updated authentication context handling

--- app/doctor/_layout.tsx
+++ app/doctor/_layout.tsx
@@ -1,42 +1,54 @@
-import 'react-native-gesture-handler'
-import React from 'react'
+import 'react-native-gesture-handler';
+import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import {Drawer} from 'expo-router/drawer'
-import CustomDrawer from '@/components/CustomDrawer';
+import Drawer from 'expo-router/drawer';
 import { DrawerContentComponentProps } from '@react-navigation/drawer';
+import CustomDrawer from '@/components/CustomDrawer';
 import { DrawerOptions } from '@/constants/drawerOptions';
-import {LinearGradient} from 'expo-linear-gradient'
+import { LinearGradient } from 'expo-linear-gradient';
+import { CustomDrawerLabel } from '@/components/CustomDrawer';
 
-const _layout = () => {
+const DoctorLayout = () => {
   return (
-    <LinearGradient colors={['#a7b9ff','#fab7c5']} dither={false} style={{flex:1}}>
-        <GestureHandlerRootView style={{flex:1}}>
-        <Drawer 
-          backBehavior='initialRoute' 
-          drawerContent={(props:DrawerContentComponentProps) => <CustomDrawer {...props}/>} 
+    <LinearGradient colors={['#a7b9ff', '#fab7c5']} dither={false} style={{ flex: 1 }}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Drawer
+          backBehavior="initialRoute"
+          drawerContent={(props: DrawerContentComponentProps) => <CustomDrawer {...props} />}
           screenOptions={DrawerOptions}
-          
-        > 
-          <Drawer.Screen name='index' options={{
-            drawerLabel:"View Patient",
-            headerTitle:"View Patient"
-          }}/>
-          <Drawer.Screen name='AddPatient' options={{
-            drawerLabel:"Add Patient",
-            headerTitle:"Add Patient"
-          }}/>
-          <Drawer.Screen name='ViewReports' options={{
-            drawerLabel:"View Report",
-            headerTitle:"View Report"
-          }}/>
-          <Drawer.Screen name='Logout' options={{
-            drawerLabel:"Logout",
-            headerTitle:"Logout"
-          }}/>
+        >
+          <Drawer.Screen
+            name="index"
+            options={{
+              drawerLabel: () => <CustomDrawerLabel iconName="people" label="View Patient" />,
+              headerTitle: "View Patient",
+            }}
+          />
+          <Drawer.Screen
+            name="AddPatient"
+            options={{
+              drawerLabel: () => <CustomDrawerLabel iconName="person-add" label="Add Patient" />,
+              headerTitle: "Add Patient",
+            }}
+          />
+          <Drawer.Screen
+            name="ViewReports"
+            options={{
+              drawerLabel: () => <CustomDrawerLabel iconName="document-text" label="View Report" />,
+              headerTitle: "View Report",
+            }}
+          />
+          <Drawer.Screen
+            name="Logout"
+            options={{
+              drawerLabel: () => <CustomDrawerLabel iconName="log-out" label="Logout" />,
+              headerTitle: "Logout",
+            }}
+          />
         </Drawer>
       </GestureHandlerRootView>
     </LinearGradient>
-  )
-}
+  );
+};
 
-export default _layout
+export default DoctorLayout;
