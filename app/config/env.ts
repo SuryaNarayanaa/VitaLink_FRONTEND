48609331547113e0b758967f30da26bb3946329f
Ananya <ananya@Ananyas-MacBook-Air.local>
2025-04-04 01:04:38 +0530

    Added Other Medications page

--- app/config/env.ts
+++ app/config/env.ts
@@ -1,5 +1,5 @@
 
-export const BASE_URL = 'http://192.168.23.209:4502'; 
+export const BASE_URL = 'http://192.168.1.5:4502'; 
 
 // Other environment variables can be added here
 export const API_TIMEOUT = 10000; // milliseconds
